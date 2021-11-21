7c00b18c06a40dd993378f13f0b0d15adadf0122
Abdulrahman-YE <63613544+Abdulrahman-YE@users.noreply.github.com>
2021-11-21 16:11:42 +0300

    handwritten-digits-classifier#utils-added

---
+++ neural-network/handwritten-digits-classifier.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np 
+from matplotlib import pyplot as plt
+import scipy
+
+# Part of Pr. Andrew Ng course Machine Leaning in coursera
+# Week 4, Exercise 3
+# In this script we will implement one-vs-all logistic regression 
+# and neural networks to recognize handwritten digits
+
+
+
