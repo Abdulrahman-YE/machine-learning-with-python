a4921fdc402a3c0c6c2c1686564111c73a926884
Abdulrahman-YE <63613544+Abdulrahman-YE@users.noreply.github.com>
2021-11-23 20:56:20 +0300

    digit_painter#nn_classifier_added

--- neural-network/digit-painter.py
+++ neural-network/digit-painter.py
@@ -2,13 +2,27 @@ from tkinter import *
 from PIL import Image , ImageDraw
 import numpy as np
 import digit_classifier
+import nn_digit_classifier
+from scipy import io
+import os
+
+#For Neural Network
+weights = io.loadmat(os.path.join('data', 'ex3weights'))
+    # get  the model weight from the dictionary
+    # Theta1 has size 25 * 401
+    # Theta2 has size 10 * 26
+Theta1, Theta2 = weights['Theta1'], weights['Theta2']
+Theta2 = np.roll(Theta2, 1, axis=0)
+#For Logistic Classifier
+all_theta = np.genfromtxt('thetas.csv', delimiter=',')
+
 
 height =100
-width = 20 
+width = 100
 
 
 #Pil empty Image
-digit = Image.new("1", (int(width), int(height/5)), 255)
+digit = Image.new("1", (int(width/5), int(height/5)), 255)
 # draw object to draw in memory only, not visable
 corrdinates = []
 img_name = 'digit.png'
@@ -42,16 +56,20 @@ def predict_img():
     # bind a drawer to the img
     draw = ImageDraw.Draw(digit_copy)
     #draw line
-    draw.line(corrdinates, 0, width=2)
+    draw.line(corrdinates, 0, width=1)
     # Get 400-dim vector that represents the grayscale intensity
-    pixels = np.array(digit_copy.getdata())
+    pixels = np.array(digit_copy.getdata()).reshape((1,-1))
     # Get all thetas from csv file
-    all_theta = np.genfromtxt('thetas.csv', delimiter=',')
-    # get predicted value
-    pred = digit_classifier.predict_one_vs_all(all_theta, pixels.reshape(1, -1))
-    predict_value.config(text=str(pred[0]))
+    # get predicted value for logistic classifier
+    pred = digit_classifier.predict_one_vs_all(all_theta, pixels)
+    l_predict_value.config(text=(" LR Pred : " + str(pred[0])))
+    pred = digit_classifier.predict_one_vs_all(all_theta, pixels)
+    # get predicted value for neural network
+    pred = nn_digit_classifier.predict(Theta1, Theta2, pixels )
+    nn_predict_value.config(text=(" NN Pred : " + str(pred[0])))
     digit_copy.save(img_name, 0)
     #Clear 
+    
     corrdinates.clear()
     canvas.delete('all')
 
@@ -63,7 +81,7 @@ window.config(bg='black')
 window.geometry(str(width)+'x' + str(height))
 
 canvas = Canvas(window)
-canvas.config(width=width, height=height/5)
+canvas.config(width=width/5, height=height/5)
 canvas.grid(row=0, column=0,columnspan=2 ,sticky='N')
 #bind an event to function
 canvas.bind('<Button-1>', locate_xy)
@@ -74,8 +92,10 @@ pred_button.grid(row=1, column=0, sticky='NWE')
 clr_button = Button(window, text='Clear', command=clear_canvas, )
 clr_button.grid(row=1, column=1, sticky='NWE')
 
-predict_value = Label(window, text='Prediction')
-predict_value.grid(row=3, column=0)
+l_predict_value = Label(window, text='NN Prediction')
+l_predict_value.grid(row=3, column=0, sticky='NSEW')
+nn_predict_value = Label(window, text='Logistic Prediction')
+nn_predict_value.grid(row=4, column=0 , sticky='NSEW')
 
 
 window.mainloop()
